716d3f23be2f2c3154910ff8603e06aa2b471799
LUCKY-DINESH <reddymca16082003@gmail.com>
2025-02-14 18:51:50 +0530

    Add files via upload

---
+++ strings_and_comaprision_and_logical_operators.py
@@ -0,0 +1,27 @@
+#strings
+x="hello"
+y="friends"
+#concatenation
+z=x+y
+print(z)
+#replication
+a=x*3
+print(a)
+
+#comparision operators
+a=20
+b=30
+c=35
+x=a>b
+y=b>=c
+z=c>=a
+print(x)
+print(y)
+print(z)
+#logical operators
+p=a>b and c>b
+q=a>b or c>b
+r=not b>c
+print(p)
+print(q)
+print(r)
