5e9f8610bdf49e4489c362b032e0d3cb5426fc12
LUCKY-DINESH <reddymca16082003@gmail.com>
2025-02-14 18:41:01 +0530

    Add files via upload

---
+++ type_coversion_and_type_of_data.py
@@ -0,0 +1,12 @@
+#type conversions that is convert one data type to another datatype
+x=int(5.7)
+y=int(8.9)
+z=float(4)
+print(x)
+print(y)
+print(z)
+#knowing the type of given data
+print(type(x))
+print(type(y))
+print(type(z))
+
