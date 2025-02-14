f1483536a5f9eb97e5c189cf3d3cf71bec69d571
LUCKY-DINESH <reddymca16082003@gmail.com>
2025-02-14 21:02:26 +0530

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,9 @@
+def fibonanci(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    else:
+        return fibonanci(n-1)+fibonanci(n-2)
+for i in range(10):
+    print(fibonanci(i))
