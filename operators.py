2c91837558f6a7ddf6d2e2ba81be9fcd985da858
LUCKY-DINESH <reddymca16082003@gmail.com>
2025-02-14 18:32:53 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,13 @@
+//sample code on average
+x=23+32+64
+y=x/3
+print(y)
+
+//Two parts of a floor need tiling. One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles //long. Tiles come in packages of 6.
+
+//How many tiles are needed?
+//You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+code:
+x=7*9+5*7
+print(x)
+print(17*6-x)
